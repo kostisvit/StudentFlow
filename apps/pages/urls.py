db95783a40252006f733d265d7de4afb3c1dc2f1
Konstantinos Vitiniotis <kostasvit@gmail.com>
2024-09-12 11:39:44 +0300

    configure url-views for login

---
+++ apps/pages/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from .views import HomePageView
+
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    #path('members/updates', UpdateAppView.as_view(), name='update_app')
+    
+]
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class HomePageView(LoginRequiredMixin,TemplateView):
+  template_name = 'home.html'
+  
+
+# class UpdateAppView(LoginRequiredMixin,TemplateView):
+#   template_name = 'app/updates/update.html'
