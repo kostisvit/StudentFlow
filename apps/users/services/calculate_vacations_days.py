fb6b5b6a480ab3234e1021cefdbd1e4d3d9bfefd
Konstantinos Vitiniotis <kostasvit@gmail.com>
2024-10-10 15:12:12 +0300

    staff vacation if enddate equeal with start date calculate one day

--- apps/users/services/calculate_vacations_days.py
+++ apps/users/services/calculate_vacations_days.py
@@ -4,5 +4,5 @@ def calculate_vacations_days(start_date, end_date):
   
   if start_date and end_date:
     delta = end_date - start_date
-    return delta.days
+    return delta.days + 1 if delta.days >= 0 else 0
   return 0
